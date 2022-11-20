44105a97c8c1e9e7c6ff320792e49c8c811838f3
Horta <Horta@DESKTOP-DQM7GVE>
2022-11-20 21:58:02 +0200

    Добавьте файлы проекта.

---
+++ 7.1/Program.cs
@@ -0,0 +1,17 @@
+﻿//Миронова Анастасия 22-ИСП-2/1
+//6.1  Уровень высокий
+//Вариант 4
+
+int FibRec(int number)
+{
+    if (number == 1 || number == 2)
+    {
+        return 1;
+    }
+    return FibRec(number - 1) + FibRec(number - 2);
+}
+
+for (var i = 1; i < 35; i++)
+{
+    Console.Write($"{FibRec(i)} ");
+}
